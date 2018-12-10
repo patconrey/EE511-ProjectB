cc32179d0b2709057b626f031a99f6846b54f5e3
Patrick Conrey <patrickconrey@gmail.com>
2018-12-09 21:41:59 -0500

    cleaned up cleanup

--- cleanup.m
+++ cleanup.m
@@ -1,7 +1,6 @@
 % This script will delete all JPEGs and MATs from the working directory
 
 close all;
-%delete *.mat
 delete FTISO_signal.mat
 delete FTISO_r.mat
 delete FTISO_Bsize.mat
@@ -9,6 +8,4 @@ delete FTISO_Bs.mat
 delete FTISO_Bcheck.mat
 delete FTISO_B.mat
 delete activegroup.mat
-
-delete *.jpg
 clear;
